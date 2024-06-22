cad8632e7681c6fcab406b2b7d2ef2e9dab72c72
Vinicius Sueiro <hi@vsueiro.com>
2024-06-21 21:34:21 -0400

    Adiciona demo de gráfico de emoji dinâmico com API do Google Sheets

---
+++ 2024/creche/script.js
@@ -0,0 +1,36 @@
+// Obter os dados
+async function carregarDados() {
+  let url =
+    "https://sheets.vsueiro.com/api/read?id=1xprmvCwcCppKDuNNPIdkJeCs8LCnk8wmPqevJsgmg-M&range=Atividade%20Creche%20Insper";
+
+  let resposta = await fetch(url);
+  let dados = await resposta.json();
+
+  desenharGrafico(dados);
+}
+
+// Desenhar gráfico
+function desenharGrafico(dados) {
+  // Monitoria categoria atual
+  let anterior = undefined;
+
+  // Para cada item dos dados
+  for (let item of dados) {
+    // Pega emoji
+    let emoji = "<span>" + item.emoji + "</span>";
+
+    // Se mudou a categoria
+    if (anterior && anterior !== item.categoria) {
+      // Adiciona quebra de linha
+      emoji = "<br><br>" + emoji;
+    }
+
+    // Atualiza valor da categoria anterior
+    anterior = item.categoria;
+
+    // Bote o emoji no parágrafo
+    creche.innerHTML += emoji;
+  }
+}
+
+carregarDados();
