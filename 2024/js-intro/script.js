8d575a78c4c8b7d50ddcccd2ae1ae752dbca5521
Vinicius Sueiro <hi@vsueiro.com>
2024-07-26 18:39:27 -0400

    Sobe mini-demo feito em aula (semestre passado)

---
+++ 2024/js-intro/script.js
@@ -0,0 +1,103 @@
+// Lista de objectos (literais)
+let pessoas = [
+  {
+    nome: "Cindy",
+    idade: 6,
+    pets: false,
+  },
+  {
+    nome: "Vini",
+    idade: 32,
+    pets: true,
+  },
+];
+
+console.log(pessoas);
+
+// let hoje = new Date()
+// .toISOString();
+// console.log(dataAtualISO);
+
+// Lista de Objetos
+// let pessoas = [
+//   {
+//     nome: "Mari",
+//     idade: 32,
+//     pets: true,
+//   },
+//   {
+//     nome: "Vini",
+//     idade: 32,
+//     pets: false,
+//   },
+// ];
+
+// console.log(pessoas);
+
+/*
+
+class GraficoBarra {
+  constructor(dados, idade, pets = false) {
+    this.nome = nome;
+    this.idade = idade;
+    this.pets = pets;
+  }
+}
+
+let pessoas = [];
+pessoas.push( new Pessoa("Cindy", 6));
+pessoas.push( new Pessoa("Vini", 32, true) );
+
+
+let hoje = new Date().toISOString();
+
+
+Variáveis podem ser declaradas com…
+- var
+- let ❤️
+- const
+
+Variáveis podem ser do tipo…
+- Número
+- Texto
+- Função
+- Lista/Array
+- Objeto
+- Elemento HTML ❤️
+
+Variáveis podem ser usadas em estruturas de controle, como:
+- Laços de repetição ❤️
+- Condições
+
+Variáveis mais úteis para dataviz são:
+Lista de objetos ❤️
+
+
+// Lista de Objetos
+let pessoas = [
+  {
+    nome: "Mari",
+    idade: 32,
+    pets: ["giovanni", "roberta"],
+    gritar: function (texto) {
+      texto = texto.toUpperCase() + "!!!";
+      console.log(texto);
+    },
+  },
+  {
+    nome: "Vini",
+    idade: 32,
+    pets: [],
+    gritar: function (texto) {
+      texto = texto.toUpperCase() + "!!!";
+      console.log(texto);
+    },
+  },
+];
+
+
+if (hoje.startsWith("2024-06-21")) {
+  document.body.innerText = "É hojeeee";
+}
+
+*/
