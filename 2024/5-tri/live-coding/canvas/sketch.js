b61a7a435e85f63f48dd213434bbb7d587efc3e5
Vinicius Sueiro <hi@vsueiro.com>
2024-08-02 21:40:17 -0400

    Adiciona exemplos feitos em sala

---
+++ 2024/5-tri/live-coding/canvas/sketch.js
@@ -0,0 +1,42 @@
+class Bolinha {
+  constructor() {
+    this.x = random(0, 300);
+    this.y = random(0, 200);
+
+    this.passoX = random(-1, 1);
+    this.passoY = random(-1, 1);
+  }
+
+  atualiza() {
+    this.x = this.x + this.passoX;
+    this.y = this.y + this.passoY;
+
+    // Checar se bati nas paredes e ir pro outro lado
+  }
+
+  desenha() {
+    circle(this.x, this.y, 6);
+  }
+}
+
+let bolinhas = [];
+
+function setup() {
+  createCanvas(300, 200);
+  noStroke();
+  fill("#aac6ca");
+
+  for (let i = 0; i < 20000; i++) {
+    let bolinha = new Bolinha();
+    bolinhas.push(bolinha);
+  }
+}
+
+function draw() {
+  background("rgba(255,255,255,1)");
+
+  for (let bolinha of bolinhas) {
+    bolinha.atualiza();
+    bolinha.desenha();
+  }
+}
