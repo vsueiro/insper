f8c03c365451fedd4a7657b42271304a9b230cb7
Vinicius Sueiro <hi@vsueiro.com>
2024-12-06 11:30:58 -0500

    Adiciona perguntas, respostas, e checagem

--- 2024/6-tri/trabalho-invisivel/script.js
+++ 2024/6-tri/trabalho-invisivel/script.js
@@ -1,1 +1,47 @@
-console.log("rodou!");
+const elementos = document.querySelectorAll(".etapa");
+const forms = document.querySelectorAll("form");
+const ranges = document.querySelectorAll('input[type="range"]');
+
+let etapa = 0;
+
+forms.forEach((form) => {
+  form.onsubmit = (event) => {
+    event.preventDefault();
+    checar(form, event.submitter);
+  };
+});
+
+function checar(form, button) {
+  const formData = button ? new FormData(form, button) : new FormData(form);
+  const formProps = Object.fromEntries(formData);
+  const resposta = formProps.resposta;
+  const correta = resposta === form.dataset.resposta;
+
+  if (correta) {
+    alert("Acertou!");
+    etapa++;
+    mostrarEtapa();
+  } else {
+    alert("Errou");
+  }
+}
+
+function mostrarValores() {
+  ranges.forEach((range) => {
+    const p = range.nextElementSibling;
+
+    range.oninput = () => {
+      p.textContent = range.value + "h";
+    };
+  });
+}
+
+mostrarValores();
+
+function mostrarEtapa() {
+  elementos.forEach((elemento, indice) => {
+    elemento.hidden = indice === etapa ? false : true;
+  });
+}
+
+mostrarEtapa();
